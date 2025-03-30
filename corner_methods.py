970b5331a5cfa9a3ad75ccd61105e2985db3c8f8
Weston Scott <westonjscott@gmail.com>
2025-03-29 21:50:09 -0700

    Actuall started final project.

---
+++ corner_methods.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+import os
+
+
+# def harris(image, args=[], corners=[]):
+#     corners = cv2.cornerHarris(image, blockSize=2, ksize=3, k=0.04)
+#     print(corners)
+#     corners = cv2.dilate(corners, None)
+#     print(corners)
+#     # corners = np.where(corners > 0.01 * corners.max())
+#     # corners = list(zip(corners[1], corners[0]))
+#     return corners
+
+def shi_tomasi(image, args=[], corners=[]):
+    
+    return corners
+
+def fast(image, args=[], corners=[]):
+    
+    return corners
+
+def orb(image, args=[], corners=[]):
+    
+    return corners
+
+def sift(image, args=[], corners=[]):
+    
+    return corners
+
+def brisk(image, args=[], corners=[]):
+    
+    return corners
+
+def agast(image, args=[], corners=[]):
+    
+    return corners
+
+def kaze(image, args=[], corners=[]):
+    
+    return corners
+
+def akaze(image, args=[], corners=[]):
+    
+    return corners
+
+def surf(image, args=[], corners=[]):
+    
+    return corners
